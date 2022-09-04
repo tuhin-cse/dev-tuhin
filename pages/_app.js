d67a4cee420393c7321fc0aca7ce45d7e85769b4
sketch <tuhin1714@cseku.ac.bd>
2022-09-04 15:04:55 +0600

    Background Animation Add

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import '../styles/app.scss'
 const App = ({Component, pageProps}) => {
     return (
         <>
+            {Array.from(new Array(15)).map((_, index) => (
+                <span className="firefly"/>
+            ))}
             <Component {...pageProps}/>
         </>
     )
